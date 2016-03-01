f29d288a1b2a11d80a66209ca48244d8f9395438
astirpe <a.stirpe@onestein.nl>
2016-03-01 08:55:02 +0100

    Add module l10n_nl_bsn

---
+++ l10n_nl_bsn/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Burgerservicenummer (BSN) for Partners',
+    'images': [],
+    'version': '8.0.0.1.0',
+    'category': 'Localization',
+    'author': 'ONESTEiN BV,Odoo Community Association (OCA)',
+    'website': 'http://www.onestein.eu',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner.xml',
+    ],
+    'external_dependencies': {
+        'python': ['stdnum'],
+    },
+    'demo': [],
+    'test': [],
+    'active': False,
+    'installable': True
+}
