2ffd08984640e307606fb0d28185d528a5886682
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2016-10-08 11:17:34 +0200

    [UPD] prefix versions with 8.0

--- l10n_nl_partner_name/__openerp__.py
+++ l10n_nl_partner_name/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Dutch partner names',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'author': 'Therp BV, Odoo Community Association (OCA)',
     'category': 'Contact management',
     'depends': [
