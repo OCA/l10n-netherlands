d94324bd6272ef68bfa0ca806907962699f45990
Andrea <a.stirpe@onestein.nl>
2017-03-10 09:53:57 +0100

    Porting, work in progress

--- l10n_nl_tax_declaration_reporting/report/report_vat.py
+++ l10n_nl_tax_declaration_reporting/report/report_vat.py
@@ -1,81 +1,75 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2014-2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models, fields
+from odoo import api, fields, models
 
 
 class VatReport(models.AbstractModel):
     _name = 'report.l10n_nl_tax_declaration_reporting.tax_report_template'
 
     @api.multi
-    def render_html(self, data=None):
-        report_model = self.env['report']._get_report_from_name(
-            'l10n_nl_tax_declaration_reporting.tax_report_template').model
+    def render_html(self, docids, data=None):
 
-        self.period_ids = []
-        _chart_tax_id = data['form']['chart_tax_id']
-        _chart_tax_name = self.env['account.tax.code'].browse(
-            _chart_tax_id).name
+        company_id = data['form']['company_id']
+        company_name = self.env['res.company'].browse(company_id).name
 
-        _fiscalyear_name = self.env['account.fiscalyear'].browse(
-            data['form']['fiscalyear_id']).name
+        date_from = data['form']['date_from']
+        date_to = data['form']['date_to']
 
-        _period_from_id = data['form']['period_from']
-        _period_from_name = 'Begin fy'
-        if _period_from_id:
-            _period_from_name = self.env['account.period'].browse(
-                _period_from_id).name
+        self.tax_report_lines = self._get_lines(data['form'])
+        self.map_tax_tag = self._map_tax_tag(data['form'])
 
-        _period_to_id = data['form']['period_to']
-        _period_to_name = 'End fy'
-        if _period_to_id:
-            _period_to_name = self.env['account.period'].browse(
-                _period_to_id).name
-
-        if data['form'].get('period_from', False) and \
-                data['form'].get('period_to', False):
-            self.period_ids = self.env['account.period'].build_ctx_periods(
-                data['form']['period_from'],
-                data['form']['period_to'])
-
-        tax_report_lines = self._get_lines(data['form']['based_on'],
-                                           data['form']['fiscalyear_id'],
-                                           data['form']['company_id'])
+        self.tags_map = {}
+        self.tags_map['1a'] = {'omzet': self.env.ref('l10n_nl.tag_nl_03').id,
+                               'btw': self.env.ref('l10n_nl.tag_nl_20').id}
+        self.tags_map['1b'] = {'omzet': self.env.ref('l10n_nl.tag_nl_05').id,
+                               'btw': self.env.ref('l10n_nl.tag_nl_22').id}
+        self.tags_map['1c'] = {'omzet': self.env.ref('l10n_nl.tag_nl_06').id,
+                               'btw': self.env.ref('l10n_nl.tag_nl_23').id}
+        self.tags_map['1d'] = {'omzet': self.env.ref('l10n_nl.tag_nl_07').id,
+                               'btw': self.env.ref('l10n_nl.tag_nl_24').id}
+        self.tags_map['1e'] = {'omzet': self.env.ref('l10n_nl.tag_nl_08').id}
+        self.tags_map['2a'] = {'omzet': self.env.ref('l10n_nl.tag_nl_10').id,
+                               'btw': self.env.ref('l10n_nl.tag_nl_27').id}
+        self.tags_map['3a'] = {'omzet': self.env.ref('l10n_nl.tag_nl_12').id}
+        self.tags_map['3b'] = {'omzet': self.env.ref('l10n_nl.tag_nl_13').id}
+        self.tags_map['3c'] = {'omzet': self.env.ref('l10n_nl.tag_nl_14').id}
+        self.tags_map['4a'] = {'omzet': self.env.ref('l10n_nl.tag_nl_16').id,
+                               'btw': self.env.ref('l10n_nl.tag_nl_29').id}
+        self.tags_map['4b'] = {'omzet': self.env.ref('l10n_nl.tag_nl_17').id,
+                               'btw': self.env.ref('l10n_nl.tag_nl_30').id}
+        self.tags_map['5b'] = {'btw': self.env.ref('l10n_nl.tag_nl_33').id}
 
         self.tax_code_error = ''
-        _1AO = self._get_line_value('1a', 'omzet', tax_report_lines)
-        _1AB = self._get_line_value('1a', 'btw', tax_report_lines)
-        _1BO = self._get_line_value('1b', 'omzet', tax_report_lines)
-        _1BB = self._get_line_value('1b', 'btw', tax_report_lines)
-        _1CO = self._get_line_value('1c', 'omzet', tax_report_lines)
-        _1CB = self._get_line_value('1c', 'btw', tax_report_lines)
-        _1DO = self._get_line_value('1d', 'omzet', tax_report_lines)
-        _1DB = self._get_line_value('1d', 'btw', tax_report_lines)
-        _1EO = self._get_line_value('1e', 'omzet', tax_report_lines)
-        _2AO = self._get_line_value('2a', 'omzet', tax_report_lines)
-        _2AB = self._get_line_value('2a', 'btw', tax_report_lines)
-        _3AO = self._get_line_value('3a', 'omzet', tax_report_lines)
-        _3BO = self._get_line_value('3b', 'omzet', tax_report_lines)
-        _3CO = self._get_line_value('3c', 'omzet', tax_report_lines)
-        _4AO = self._get_line_value('4a', 'omzet', tax_report_lines)
-        _4AB = self._get_line_value('4a', 'btw', tax_report_lines)
-        _4BO = self._get_line_value('4b', 'omzet', tax_report_lines)
-        _4BB = self._get_line_value('4b', 'btw', tax_report_lines)
-        _5BB = self._get_line_value('5b', 'btw', tax_report_lines)
+        _1AO = self._get_line_value('1a', 'omzet')
+        _1AB = self._get_line_value('1a', 'btw')
+        _1BO = self._get_line_value('1b', 'omzet')
+        _1BB = self._get_line_value('1b', 'btw')
+        _1CO = self._get_line_value('1c', 'omzet')
+        _1CB = self._get_line_value('1c', 'btw')
+        _1DO = self._get_line_value('1d', 'omzet')
+        _1DB = self._get_line_value('1d', 'btw')
+        _1EO = self._get_line_value('1e', 'omzet')
+        _2AO = self._get_line_value('2a', 'omzet')
+        _2AB = self._get_line_value('2a', 'btw')
+        _3AO = self._get_line_value('3a', 'omzet')
+        _3BO = self._get_line_value('3b', 'omzet')
+        _3CO = self._get_line_value('3c', 'omzet')
+        _4AO = self._get_line_value('4a', 'omzet')
+        _4AB = self._get_line_value('4a', 'btw')
+        _4BO = self._get_line_value('4b', 'omzet')
+        _4BB = self._get_line_value('4b', 'btw')
+        _5BB = self._get_line_value('5b', 'btw')
 
         _5AB = _1AB + _1BB + _1CB + _1DB + _2AB - _4AB - _4BB
         _5CB = _5AB - _5BB
 
         docargs = {
-            'doc_ids': self._ids,
-            'doc_model': report_model,
-            'docs': self,
             'time_now': fields.Datetime.now(),
-            'chart': _chart_tax_name,
-            'fiscalyear': _fiscalyear_name,
-            'start_period': _period_from_name,
-            'end_period': _period_to_name,
+            'company': company_name,
+            'start_date': date_from,
+            'end_date': date_to,
             '_1AO': _1AO,
             '_1AB': _1AB,
             '_1BO': _1BO,
@@ -103,68 +97,65 @@ class VatReport(models.AbstractModel):
         return self.env['report'].render(
             'l10n_nl_tax_declaration_reporting.tax_report_template', docargs)
 
-    def _get_line_value(self, code, code_type, tax_report_lines):
-        try:
-            return filter(lambda x: x[0] == code,
-                          filter(lambda x: x[1] == code_type,
-                                 tax_report_lines))[0][2]
-        except IndexError:
-            if not self.tax_code_error:
-                self.tax_code_error = \
-                    "Please add (BTW) or (omzet) to tax code: " + code
+    def _get_line_value(self, code, type):
+        tax_report_lines = self.tax_report_lines
+        map_tax_tag = self.map_tax_tag
+        tag_id = self.tags_map[code][type]
+        for line in tax_report_lines:
+            tax_id = line[0]
+            tags_list = map_tax_tag[tax_id]
+            if tag_id in tags_list:
+                if type == 'omzet':
+                    return line[2]
+                return line[1]
+        return 0.0
+
+    def _map_tax_tag(self, form):
+
+        query = """
+            SELECT account_tax_id, account_account_tag_id
+            FROM account_tax_account_tag
+        """
+        self._cr.execute(query)
+
+        res = self._cr.fetchall()
+        map = {}
+        for row in res:
+            key = row[0]
+            if row[0] not in map:
+                map[key] = [row[1]]
             else:
-                self.tax_code_error += " and " + code
-            return 0
+                map[key].append(row[1])
+        return map
 
-    def _get_lines(self, based_on, fiscalyear_id, company_id=False,
-                   parent=False, level=0):
-        period_list = self.period_ids
-        account_list = self._get_codes(based_on, company_id, parent, level)
-        if not period_list:
-            if fiscalyear_id:
-                f_year = self.env['account.fiscalyear'].browse(fiscalyear_id)
-                period_list = f_year.period_ids.ids
-            else:
-                period_list = self.env['account.period'].search([]).ids
-        res = self._add_codes(based_on, account_list, period_list)
+    def _get_lines(self, form):
 
-        tax_report_lines = list(map(lambda x: (x[1].code, 'omzet'
-                                    if '(omzet)' in x[1].name
-                                    else 'btw' if '(BTW)' in x[1].name
-                                    else 'error', int(round(x[2]))), res))
+        def filter_by_dates(date_from, date_to):
+            where = 'WHERE inv.state IN (\'open\',\'paid\') '
+            if date_from:
+                where += " and inv.date >=  '" + date_from + "'"
+            if date_to:
+                where += " and inv.date <=  '" + date_to + "'"
+            return where
 
-        return tax_report_lines
+        query = """
+            SELECT inv_tax.tax_id,
+                   sum(inv_tax.amount),
+                   sum(inv_line.price_subtotal_signed)
+            FROM account_invoice_tax AS inv_tax
+                JOIN account_invoice_line_tax inv_line_tax
+                    on inv_line_tax.tax_id = inv_tax.tax_id
+                JOIN account_invoice_line inv_line
+                    on inv_line.id = inv_line_tax.invoice_line_id
+                JOIN account_invoice inv
+                    on inv.id = inv_line.invoice_id
+        """
+        query += filter_by_dates(form['date_from'], form['date_to'])
 
-    def _get_codes(self, based_on, company_id, parent=False, level=0):
-        tax_code_obj = self.env['account.tax.code']
-        tax_codes = tax_code_obj.with_context(based_on=based_on).search([
-            ('parent_id', '=', parent),
-            ('company_id', '=', company_id)
-        ], order='sequence')
+        query += """
+            AND inv.company_id = %s
+            GROUP BY inv_tax.tax_id
+        """
+        self._cr.execute(query, (form['company_id'],))
 
-        res = []
-        for code in tax_codes:
-            res.append(('.' * 2 * level, code))
-
-            res += self._get_codes(based_on, company_id, code.id, level + 1)
-        return res
-
-    def _add_codes(self, based_on, account_list=None, period_list=None):
-        if account_list is None:
-            account_list = []
-        if period_list is None:
-            period_list = []
-        res = []
-        for account in account_list:
-            sum_tax_add = 0
-            for period_ind in period_list:
-                for code in self.env['account.tax.code'].with_context(
-                    period_id=period_ind,
-                    based_on=based_on
-                ).browse(account[1].id):
-                    sum_tax_add += code.sum_period
-
-            code.sum_period = sum_tax_add
-
-            res.append((account[0], code, sum_tax_add))
-        return res
+        return self._cr.fetchall()
