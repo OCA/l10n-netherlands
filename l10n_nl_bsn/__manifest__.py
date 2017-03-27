84f1402937cd115b7d9f29150dfa2113ec5c922d
Andrea <a.stirpe@onestein.nl>
2017-03-27 16:00:18 +0200

    Code review

--- l10n_nl_bsn/__manifest__.py
+++ l10n_nl_bsn/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016-2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -7,11 +7,10 @@
     'images': [],
     'version': '10.0.0.1.0',
     'category': 'Localization',
-    'author': 'ONESTEiN BV,Odoo Community Association (OCA)',
+    'author': 'Onestein, Odoo Community Association (OCA)',
     'website': 'http://www.onestein.eu',
     'license': 'AGPL-3',
     'depends': [
-        'base',
         'hr',
     ],
     'data': [
