fb97125613f247a01a93bb540f685deb3f87a77f
Andrea Stirpe <a.stirpe@onestein.nl>
2017-05-18 11:55:54 +0200

    [10.0] Add tests for l10n_nl_bsn (#73)
    * Add tests

--- l10n_nl_bsn/models/res_partner.py
+++ l10n_nl_bsn/models/res_partner.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016-2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 from odoo.tools.translate import _
 
 _logger = logging.getLogger(__name__)
@@ -19,7 +19,8 @@ class ResPartner(models.Model):
 
     bsn_number = fields.Char(
         string='BSN',
-        groups='hr.group_hr_user')
+        groups='hr.group_hr_user'
+    )
 
     @api.multi
     @api.onchange('bsn_number')
@@ -31,26 +32,39 @@ class ResPartner(models.Model):
                 partner.bsn_number = bsn.format(partner.bsn_number)
 
                 # check is valid, otherwise display a warning
-                if not bsn.is_valid(partner.bsn_number):
-                    msg = _('The BSN you entered (%s) is not valid.')
-                    warning = {
-                        'title': _('Warning!'),
-                        'message': msg % partner.bsn_number,
-                    }
+                warning = partner._warn_bsn_invalid()
 
                 # search for another partner with the same BSN
                 args = [('bsn_number', '=', partner.bsn_number),
                         ('name', '!=', partner.name)]
+
                 # refine search in case of multicompany setting
                 if partner.company_id:
                     args += [('company_id', '=', partner.company_id.id)]
                 other_partner = self.search(args, limit=1)
+
                 # if another partner exists, display a warning
                 if other_partner:
-                    msg = _('Another person (%s) has the same BSN (%s).')
-                    warning = {
-                        'title': _('Warning!'),
-                        'message': msg % (other_partner.name,
-                                          other_partner.bsn_number)
-                    }
+                    warning = other_partner._warn_bsn_existing()
         return {'warning': warning, }
+
+    @api.multi
+    def _warn_bsn_invalid(self):
+        self.ensure_one()
+        warning = {}
+        if not bsn.is_valid(self.bsn_number):
+            msg = _('The BSN you entered (%s) is not valid.')
+            warning = {
+                'title': _('Warning!'),
+                'message': msg % self.bsn_number,
+            }
+        return warning
+
+    def _warn_bsn_existing(self):
+        self.ensure_one()
+        msg = _('Another person (%s) has the same BSN (%s).')
+        warning = {
+            'title': _('Warning!'),
+            'message': msg % (self.name, self.bsn_number)
+        }
+        return warning
