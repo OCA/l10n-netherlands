1ea0a6958ebc78d15467142219a20cc1cbf2d879
Erwin van der Ploeg <erwin@odooexperts.nl>
2017-06-29 10:35:11 +0200

    New module l10n_nl_banks

---
+++ l10n_nl_banks/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Odoo Experts
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Dutch banks",
+    "version": "10.0.1.0.0",
+    "author": "Odoo Experts, Onestein, Odoo Community Association (OCA)",
+    "website": "https://www.odooexperts.nl",
+    "category": "Accounting",
+    "depends": ["base"],
+    "summary": "Import all Dutch banks with BIC code",
+    "data": [
+    		"data/nl_banks.xml"
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
