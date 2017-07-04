3bdb060502bb3c08569ea2a328a8321476d24263
Erwin van der Ploeg <erwin@odooexperts.nl>
2017-07-04 08:33:41 +0200

    New module l10n_nl_banks (#92)
    * New module l10n_nl_banks
    
    * Remove blanc sections
    
    * Icon credits
    
    * OCA Requested changes
    
    * Change module name
    
    * Changes to comply with OCA

---
+++ l10n_nl_bank/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Odoo Experts (<https://www.odooexperts.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Dutch banks list",
+    "summary": "Import all Dutch banks with BIC code",
+    "version": "10.0.1.0.0",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "Odoo Experts, Odoo Community Association (OCA)",
+    "website": "https://www.odooexperts.nl",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "data/res_bank_data.xml"
+    ],
+    "installable": True,
+}
