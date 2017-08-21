e46c12e0d077980d23a2e67a1f90e8e57b462c37
Andrea <a.stirpe@onestein.nl>
2017-08-21 08:34:49 +0200

    Remove obsolete code from l10n_nl_tax_invoice_basis

--- l10n_nl_tax_invoice_basis/models/account_move_line.py
+++ l10n_nl_tax_invoice_basis/models/account_move_line.py
@@ -9,21 +9,3 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     tax_date = fields.Date()
-
-    date = fields.Date(
-        related=False,
-        compute='_compute_date',
-        index=True,
-        copy=False,
-        store=True
-    )
-
-    @api.depends('move_id.date', 'tax_date', 'company_id.country_id')
-    @api.multi
-    def _compute_date(self):
-        for line in self:
-            line.date = line.move_id.date
-            is_invoice_basis = line.company_id.l10n_nl_tax_invoice_basis
-            is_nl = line.company_id.country_id == self.env.ref('base.nl')
-            if is_nl and is_invoice_basis and line.tax_line_id:
-                line.date = line.tax_date or line.move_id.date
