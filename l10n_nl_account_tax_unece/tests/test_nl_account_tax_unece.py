4d2505b71a31de5ab9bc7879f70d2ec00e30ef0c
Andrea <a.stirpe@onestein.nl>
2017-09-05 16:00:31 +0200

    Add tests + code review

--- l10n_nl_account_tax_unece/tests/test_nl_account_tax_unece.py
+++ l10n_nl_account_tax_unece/tests/test_nl_account_tax_unece.py
@@ -7,16 +7,17 @@ from odoo.tests.common import TransactionCase
 
 class TestNlAccountTaxUnece(TransactionCase):
 
-    def test_load_coa(self):
-        my_company = self.env['res.company'].create({
+    def setUp(self):
+        super(TestNlAccountTaxUnece, self).setUp()
+
+        self.my_company = self.env['res.company'].create({
             'name': 'My Dutch Company',
             'country_id': self.env.ref('base.nl').id,
-
         })
         l10nnl_chart_template = self.env.ref('l10n_nl.l10nnl_chart_template')
         transfer_account_id = self.env.ref('l10n_nl.transfer_account_id')
-        wizard = self.env['wizard.multi.charts.accounts'].create({
-            'company_id': my_company.id,
+        self.wizard = self.env['wizard.multi.charts.accounts'].create({
+            'company_id': self.my_company.id,
             'chart_template_id': l10nnl_chart_template.id,
             'transfer_account_id': transfer_account_id.id,
             'code_digits': 6,
@@ -25,17 +26,13 @@ class TestNlAccountTaxUnece(TransactionCase):
             'sale_tax_rate': 21.0,
             'purchase_tax_rate': 21.0,
             'complete_tax_set': True,
-            'currency_id': my_company.currency_id.id,
+            'currency_id': self.my_company.currency_id.id,
             'bank_account_code_prefix': '103',
             'cash_account_code_prefix': '101',
         })
-        wizard.execute()
 
-        taxes = self.env['account.tax'].search([
-            ('company_id', '=', my_company.id)
-        ])
-        unece_type_id = self.env.ref('account_tax_unece.tax_type_vat').id
-        unece_categ_ids = [
+        self.unece_type_id = self.env.ref('account_tax_unece.tax_type_vat').id
+        self.unece_categ_ids = [
             self.env.ref('account_tax_unece.tax_categ_h').id,
             self.env.ref('account_tax_unece.tax_categ_aa').id,
             self.env.ref('account_tax_unece.tax_categ_z').id,
@@ -43,6 +40,45 @@ class TestNlAccountTaxUnece(TransactionCase):
             self.env.ref('account_tax_unece.tax_categ_b').id,
             self.env.ref('account_tax_unece.tax_categ_e').id
         ]
+
+    def test_load_coa(self):
+
+        # Set the Dutch chart of accounts for the company
+        self.wizard.execute()
+
+        unece_type_id = self.unece_type_id
+        unece_categ_ids = self.unece_categ_ids
+
+        taxes = self.env['account.tax'].search([
+            ('company_id', '=', self.my_company.id)
+        ])
+
+        # For each tax account verify that the UNECE values are set
+        for tax in taxes:
+            self.assertTrue(tax.unece_type_id.id == unece_type_id)
+            self.assertTrue(tax.unece_categ_id.id in unece_categ_ids)
+
+    def test_existing_coa_update(self):
+
+        # Set the Dutch chart of accounts for the company
+        self.wizard.execute()
+
+        taxes = self.env['account.tax'].search([
+            ('company_id', '=', self.my_company.id)
+        ])
+
+        # For each tax account, force the UNECE values to False
+        for tax in taxes:
+            tax.unece_type_id = False
+            tax.unece_categ_id = False
+
+        # Launch the method that is normally executed by the post_install.py
+        self.my_company._l10n_nl_set_unece_on_taxes()
+
+        unece_type_id = self.unece_type_id
+        unece_categ_ids = self.unece_categ_ids
+
+        # For each tax account verify that the UNECE values are set
         for tax in taxes:
             self.assertTrue(tax.unece_type_id.id == unece_type_id)
             self.assertTrue(tax.unece_categ_id.id in unece_categ_ids)
