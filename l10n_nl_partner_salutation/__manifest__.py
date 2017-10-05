bc759d457dd454b84b0ec2fac8a200c6e84f9f2e
Ronald Portier <ronald@therp.nl>
2017-10-05 11:58:47 +0200

    [IMP] l10n_nl_partner_salutation. Base module on partner_contact_gender.
    - Do not add gender field in this module, but rely on partner_contact_gender.
    - Use standard directories.
    - Make sure tests use same name formatting as in model.
    - make test class and method name confirm to actual tests done.
    - Properly handle is_company partners.
    - Make a difference between unknown gender and other gender.

--- l10n_nl_partner_salutation/__manifest__.py
+++ l10n_nl_partner_salutation/__manifest__.py
@@ -3,16 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Full salutation for partners, Dutch style',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'author': 'Therp BV, Odoo Community Association (OCA)',
     'category': 'Contact management',
     "license": "AGPL-3",
     'depends': [
+        'partner_contact_gender',
         'l10n_nl_partner_name',
     ],
     'data': [
         'data/res_partner_title.xml',
-        'view/res_partner_title.xml',
-        'view/res_partner.xml',
-        ],
+        'views/res_partner_title.xml',
+        'views/res_partner.xml',
+    ],
 }
