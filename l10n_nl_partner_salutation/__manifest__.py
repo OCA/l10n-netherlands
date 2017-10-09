86b6fe646968e5379787b22a2caa62785c7f0102
Ronald Portier <ronald@therp.nl>
2017-10-09 23:10:34 +0200

    [FIX] Impprovements after preview.
    + Standard copyright headers
    + Migration of 'unknown' gender value to NULL.

--- l10n_nl_partner_salutation/__manifest__.py
+++ l10n_nl_partner_salutation/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2017 Therp BV <http://therp.nl>
+# Copyright 2017 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Full salutation for partners, Dutch style',
     'version': '10.0.1.1.0',
     'author': 'Therp BV, Odoo Community Association (OCA)',
     'category': 'Contact management',
-    "license": "AGPL-3",
+    'license': 'AGPL-3',
+    'website': 'https://github.com/oca/l10n-netherlands.git',  
     'depends': [
         'partner_contact_gender',
         'l10n_nl_partner_name',
