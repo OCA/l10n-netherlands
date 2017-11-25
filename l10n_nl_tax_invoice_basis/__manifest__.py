b9f000dc4aa7adc903bc67cf4ca6b5f032347fd6
Andrea <a.stirpe@onestein.nl>
2017-11-25 12:23:10 +0100

    Port l10n_nl_tax_invoice_basis to V11

--- l10n_nl_tax_invoice_basis/__manifest__.py
+++ l10n_nl_tax_invoice_basis/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'NL Tax Invoice Basis (Factuurstelsel)',
     'summary': 'Enable invoice basis on tax according to the Dutch law',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Localization',
     'author': 'Onestein, Odoo Community Association (OCA)',
     'website': 'http://www.onestein.eu',
@@ -15,6 +14,7 @@
         'account_tax_balance',
     ],
     'data': [
-        'views/account_config_settings.xml',
-    ]
+        'views/res_config_settings.xml',
+    ],
+    'installable': True,
 }
