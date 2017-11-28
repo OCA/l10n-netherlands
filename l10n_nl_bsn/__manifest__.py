fab0ed2ea563e1ce9e3a95c129b15e4613f78de8
Andrea <a.stirpe@onestein.nl>
2017-11-28 08:53:31 +0100

    Porting to V11

--- l10n_nl_bsn/__manifest__.py
+++ l10n_nl_bsn/__manifest__.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Burgerservicenummer (BSN) for Partners',
-    'images': [],
-    'version': '10.0.0.1.0',
+    'version': '11.0.1.0.0',
     'category': 'Localization',
     'author': 'Onestein, Odoo Community Association (OCA)',
     'website': 'http://www.onestein.eu',
