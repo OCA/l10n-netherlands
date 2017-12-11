654859ed6ce79aca2c303b05f7b56f5853c44d32
Ronald Portier <ronald@therp.nl>
2017-12-11 15:04:59 +0100

    [FIX] l10n_nl_partner_salutation. Fix Travis errors.

--- l10n_nl_partner_salutation/migrations/10.0.1.1.0/post-migration.py
+++ l10n_nl_partner_salutation/migrations/10.0.1.1.0/post-migration.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Therp BV <https://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).                   
-                                                                                  
-                                                                                  
-def migrate(cr, version):                                                         
-                                                                                  
-    cr.execute(                                                                   
-        "UPDATE res_partner SET gender = NULL where gender = 'unknown'"        
-    ) 
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def migrate(cr, version):
+
+    cr.execute(
+        "UPDATE res_partner SET gender = NULL where gender = 'unknown'"
+    )
