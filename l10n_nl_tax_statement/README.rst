b14c567d2223b0f0c9c8fa2150986934643db612
Andrea <a.stirpe@onestein.nl>
2018-01-16 08:52:42 +0100

    [11.0][IMP] Include past undeclared invoices

--- l10n_nl_tax_statement/README.rst
+++ l10n_nl_tax_statement/README.rst
@@ -42,9 +42,17 @@ To create a statement you need to:
 #. Manually enter the BTW amounts of lines '5d', '5e', '5f' (in Edit mode, click on the amount of the line to be able to change it)
 #. Press the Post button to set the status of the statement to Posted; the statements set to this state cannot be modified
 
+To add past undeclared invoices:
+
+#. Open the tab `Past Undeclared Invoices`, available when the statement is in status Draft.
+#. Set an initial date (field From Date) from which the past undeclared invoices will be displayed.
+#. One by one, add the displayed undeclared invoices, by clicking on the `Add Invoice` button present in each line.
+#. Press the Update button in order to recompute the statement lines.
+
 Extra info about the workflow:
 
 #. If you need to recalculate or modify or delete a statement already set to Posted status you need first to set it back to Draft status: press the button Reset to Draft
+#. Instead, if you send the statement to the Tax Authority, you may want to avoid that the statement is set back to Draft: to avoid this, press the button Final. If you then confirm, it will be not possible to modify this Statement or reset it back to draft anymore.
 
 Printing a PDF report:
 
@@ -60,7 +68,6 @@ Known issues / Roadmap
 ======================
 
 * Exporting in SBR/XBLR format not yet available
-* Including in the report being created, not only the data filtered by the selected date range, but also all the old data that was not included in the previous tax declarations (work in progress...)
 
 Bug Tracker
 ===========
--- l10n_nl_tax_statement/tests/test_l10n_nl_vat_statement.py
+++ l10n_nl_tax_statement/tests/test_l10n_nl_vat_statement.py
@@ -1,9 +1,13 @@
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.exceptions import Warning as UserError
-from odoo.tests.common import TransactionCase
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields
+from odoo.exceptions import UserError
+from odoo.tests.common import TransactionCase, at_install, post_install
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
 
 
 class TestVatStatement(TransactionCase):
@@ -82,7 +86,7 @@ class TestVatStatement(TransactionCase):
             'partner_id': self.partner.id,
             'account_id': invoice_line_account,
             'journal_id': self.journal_1.id,
-            'date_invoice': datetime.today(),
+            'date_invoice': fields.Date.today(),
             'type': 'out_invoice',
         })
 
@@ -116,20 +120,55 @@ class TestVatStatement(TransactionCase):
             [self.statement_1.from_date, self.statement_1.to_date])
         self.assertEqual(self.statement_1.name, check_name)
 
-    def test_02_post(self):
+        self.statement_1.onchange_date_from_date()
+        d_from = datetime.strptime(self.statement_1.from_date, DF)
+        # by default the unreported_move_from_date is set to
+        # a quarter (three months) before the from_date of the statement
+        d_from_2months = d_from + relativedelta(months=-3, day=1)
+        new_date = fields.Date.to_string(d_from_2months)
+        self.assertEqual(self.statement_1.unreported_move_from_date, new_date)
+
+        self.assertEqual(self.statement_1.btw_total, 0.)
+
+    def test_02_post_final(self):
+        # first post
+        self.statement_1.statement_update()
         self.statement_1.post()
         self.assertEqual(self.statement_1.state, 'posted')
         self.assertTrue(self.statement_1.date_posted)
+
+        # then finalize
+        self.statement_1.finalize()
+        self.assertEqual(self.statement_1.state, 'final')
+        self.assertTrue(self.statement_1.date_posted)
+
+        with self.assertRaises(UserError):
+            self.statement_1.write({'name': 'Test Name Modified'})
+        with self.assertRaises(UserError):
+            self.statement_1.write({'state': 'posted'})
+        with self.assertRaises(UserError):
+            self.statement_1.write({'date_posted': fields.Datetime.now()})
+        with self.assertRaises(UserError):
+            self.statement_1.unlink()
+        for line in self.statement_1.line_ids:
+            with self.assertRaises(UserError):
+                line.unlink()
+
+        self.assertEqual(self.statement_1.btw_total, 0.)
 
     def test_03_reset(self):
         self.statement_1.reset()
         self.assertEqual(self.statement_1.state, 'draft')
         self.assertFalse(self.statement_1.date_posted)
 
+        self.assertEqual(self.statement_1.btw_total, 0.)
+
     def test_04_write(self):
         self.statement_1.post()
         with self.assertRaises(UserError):
             self.statement_1.write({'name': 'Test Name'})
+
+        self.assertEqual(self.statement_1.btw_total, 0.)
 
     def test_05_unlink_exception(self):
         self.statement_1.post()
@@ -166,10 +205,15 @@ class TestVatStatement(TransactionCase):
         self.assertFalse(_1.format_omzet)
         self.assertFalse(_1.format_btw)
         self.assertTrue(_1.is_group)
+        self.assertTrue(_1.is_readonly)
 
         self.assertEqual(_1a.format_omzet, '100.00')
         self.assertEqual(_1a.format_btw, '10.50')
         self.assertFalse(_1a.is_group)
+        self.assertTrue(_1.is_readonly)
+
+        self.assertEqual(self.statement_1.btw_total, 10.5)
+        self.assertEqual(self.statement_1.format_btw_total, '10.50')
 
     def test_10_line_unlink_exception(self):
         self.invoice_1.action_invoice_open()
@@ -177,6 +221,11 @@ class TestVatStatement(TransactionCase):
         self.statement_1.post()
         with self.assertRaises(UserError):
             self.statement_1.line_ids.unlink()
+
+        self.assertEqual(self.statement_1.btw_total, 0.)
+
+        for line in self.statement_1.line_ids:
+            self.assertTrue(line.is_readonly)
 
     def test_11_wizard_execute(self):
         wizard = self.Wizard.create({})
@@ -208,3 +257,115 @@ class TestVatStatement(TransactionCase):
         self.assertTrue(config)
         self.assertEqual(config.tag_1a_btw, self.tag_1)
         self.assertEqual(config.tag_1a_omzet, self.tag_2)
+
+        self.assertEqual(self.statement_1.btw_total, 0.)
+
+    def test_12_undeclared_invoice(self):
+        self.invoice_1._onchange_invoice_line_ids()
+        self.invoice_1.action_invoice_open()
+        self.invoice_1.move_id.add_move_in_statement()
+        for line in self.invoice_1.move_id.line_ids:
+            self.assertEqual(line.l10n_nl_vat_statement_include, True)
+        self.invoice_1.move_id.unlink_move_from_statement()
+        for line in self.invoice_1.move_id.line_ids:
+            self.assertEqual(line.l10n_nl_vat_statement_include, False)
+
+        self.statement_1.statement_update()
+        self.assertEqual(len(self.statement_1.line_ids.ids), 22)
+        self.statement_1.post()
+
+        invoice2 = self.invoice_1.copy()
+        invoice2._onchange_invoice_line_ids()
+        invoice2.action_invoice_open()
+        statement2 = self.Statement.create({
+            'name': 'Statement 2',
+        })
+        statement2.statement_update()
+        statement2.unreported_move_from_date = fields.Date.today()
+        statement2.onchange_unreported_move_from_date()
+        self.assertFalse(statement2.unreported_move_ids)
+
+        self.assertEqual(self.statement_1.btw_total, 10.5)
+        self.assertEqual(self.statement_1.format_btw_total, '10.50')
+
+        for line in self.statement_1.line_ids:
+            self.assertTrue(line.is_readonly)
+
+    def test_13_no_previous_statement_posted(self):
+        statement2 = self.Statement.create({
+            'name': 'Statement 2',
+        })
+        statement2.statement_update()
+        with self.assertRaises(UserError):
+            statement2.post()
+
+        self.assertEqual(self.statement_1.btw_total, 0.)
+        self.assertEqual(self.statement_1.format_btw_total, '0.00')
+
+        for line in self.statement_1.line_ids:
+            self.assertFalse(line.is_readonly)
+
+    @at_install(False)
+    @post_install(True)
+    def test_14_is_invoice_basis(self):
+        company = self.statement_1.company_id
+        has_invoice_basis = self.env['ir.model.fields'].sudo().search_count([
+            ('model', '=', 'res.company'),
+            ('name', '=', 'l10n_nl_tax_invoice_basis')
+        ])
+        if has_invoice_basis:
+            company.l10n_nl_tax_invoice_basis = True
+            self.statement_1._compute_is_invoice_basis()
+            self.assertEqual(self.statement_1.is_invoice_basis, True)
+            company.l10n_nl_tax_invoice_basis = False
+            self.statement_1._compute_is_invoice_basis()
+            self.assertEqual(self.statement_1.is_invoice_basis, False)
+
+        self.assertEqual(self.statement_1.btw_total, 0.)
+        self.assertEqual(self.statement_1.format_btw_total, '0.00')
+
+        for line in self.statement_1.line_ids:
+            self.assertTrue(line.is_readonly)
+
+    @at_install(False)
+    @post_install(True)
+    def test_15_invoice_basis_undeclared_invoice(self):
+        self.invoice_1._onchange_invoice_line_ids()
+        self.invoice_1.action_invoice_open()
+        self.statement_1.statement_update()
+        self.assertEqual(len(self.statement_1.line_ids.ids), 22)
+        self.statement_1.with_context(
+            skip_check_config_tag_3b_omzet=True
+        ).post()
+
+        has_invoice_basis = self.env['ir.model.fields'].sudo().search_count([
+            ('model', '=', 'res.company'),
+            ('name', '=', 'l10n_nl_tax_invoice_basis')
+        ])
+        if has_invoice_basis:
+
+            self.statement_1.company_id.l10n_nl_tax_invoice_basis = True
+            self.statement_1.company_id.country_id = self.env.ref('base.nl')
+
+            invoice2 = self.invoice_1.copy()
+            d_date = datetime.strptime(fields.Date.today(), DF)
+            d_date = d_date + relativedelta(months=-4, day=1)
+            old_date = fields.Date.to_string(d_date)
+            invoice2.date_invoice = old_date
+            invoice2.action_invoice_open()
+
+            statement2 = self.Statement.create({
+                'name': 'Statement 2',
+            })
+            statement2.unreported_move_from_date = fields.Date.today()
+            statement2.onchange_unreported_move_from_date()
+            statement2.statement_update()
+            statement2.with_context(
+                skip_check_config_tag_3b_omzet=True
+            ).post()
+
+        self.assertEqual(self.statement_1.btw_total, 10.5)
+        self.assertEqual(self.statement_1.format_btw_total, '10.50')
+
+        for line in self.statement_1.line_ids:
+            self.assertTrue(line.is_readonly)
