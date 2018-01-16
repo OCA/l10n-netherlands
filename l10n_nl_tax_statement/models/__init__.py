40b4328ff4c736210fac9a58e4ceadc010c93cad
Andrea <a.stirpe@onestein.nl>
2018-01-16 08:52:42 +0100

    [11.0][IMP] Include past undeclared invoices

--- l10n_nl_tax_statement/models/__init__.py
+++ l10n_nl_tax_statement/models/__init__.py
@@ -1,3 +1,6 @@
 from . import l10n_nl_vat_statement
 from . import l10n_nl_vat_statement_line
 from . import l10n_nl_vat_statement_config
+from . import account_move
+from . import account_move_line
+from . import account_tax
