9d89b2061bff501b40c99c65774c0d314a16e128
Andrea <a.stirpe@onestein.nl>
2018-01-17 15:05:30 +0100

    [11.0][MIG] l10n_nl_account_tax_unece

--- l10n_nl_account_tax_unece/__init__.py
+++ l10n_nl_account_tax_unece/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import models
 from .post_install import set_unece_on_taxes
--- l10n_nl_account_tax_unece/__manifest__.py
+++ l10n_nl_account_tax_unece/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'L10n NL Account Tax UNECE',
     'summary': 'Auto-configure UNECE params on Dutch taxes',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Localization',
     'author': 'Onestein, Odoo Community Association (OCA)',
     'website': 'http://www.onestein.eu',
@@ -16,5 +15,5 @@
     ],
     'post_init_hook': 'set_unece_on_taxes',
     'installable': True,
-    'auto_installable': True,
+    'auto_install': True,
 }
