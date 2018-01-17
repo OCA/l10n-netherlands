5d34c9dec5864fd8033dfa1d0715748ed4cb5689
Andrea <a.stirpe@onestein.nl>
2018-01-17 15:05:30 +0100

    [11.0][MIG] l10n_nl_account_tax_unece

--- l10n_nl_account_tax_unece/post_install.py
+++ l10n_nl_account_tax_unece/post_install.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
