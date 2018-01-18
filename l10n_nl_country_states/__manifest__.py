db71c3cc905196fb85b1058d08717c9d925f1460
lfreeke <lfreeke@therp.nl>
2018-01-18 13:13:29 +0100

    [ADD] Add state module
    [MOD] Add iso standard codes and the ABC islands

---
+++ l10n_nl_country_states/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "l10n_nl_country_states",
+    "version": "10.0.1.0.1",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Dutch Localization",
+    "depends": [
+        'base',
+    ],
+    "data": [
+        'data/l10n_nl_country_states.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
