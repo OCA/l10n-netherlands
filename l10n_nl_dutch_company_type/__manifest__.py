7a2b37b7abb1abfbff1ce5fedbdf8a5ca8acbf75
lfreeke <lfreeke@therp.nl>
2018-01-18 14:44:28 +0100

    :[ADD] Add company types data module
    [FIX] noupdate to 1
    
    [FIX] http -> https and added summary

---
+++ l10n_nl_dutch_company_type/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "l10n_nl_dutch_company_type",
+    "version": "10.0.1.0.0",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Dutch localization",
+    "summary": "Adds the Dutch company types",
+    "depends": [
+        'partner_company_type',
+    ],
+    "data": [
+        'data/l10n_nl_dutch_company_type.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
