c3a6b121533328584b4c6c2c93562ed935cbac00
Holger Brunn <hbrunn@therp.nl>
2018-01-29 19:05:33 +0100

    [ADD] look up province from zip code

---
+++ l10n_nl_country_states/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2018 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import res_country_state_nl_zip
+from . import res_partner
---
+++ l10n_nl_country_states/models/res_country_state_nl_zip.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2018 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
+
+
+class ResCountryStateNlZip(models.Model):
+    _name = 'res.country.state.nl.zip'
+    _description = 'Map zip code areas to states'
+
+    state_id = fields.Many2one('res.country.state', required=True)
+    min_zip = fields.Integer('Low boundary of zip code area', required=True)
+    max_zip = fields.Integer('High boundary of zip code area', required=True)
