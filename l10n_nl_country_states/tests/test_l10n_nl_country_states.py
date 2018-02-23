0ec08aaee83891e48284655fe1076e985bd5af63
Ronald Portier <ronald@therp.nl>
2018-02-23 15:26:51 +0100

    [FIX] l10n_nl_country_states. contry_id -> country_id in tests.

--- l10n_nl_country_states/tests/test_l10n_nl_country_states.py
+++ l10n_nl_country_states/tests/test_l10n_nl_country_states.py
@@ -15,13 +15,13 @@ class TestL10nNlCountryStates(TransactionCase):
             self.env.ref('l10n_nl_country_states.state_noordholland'),
         )
         partner.write({'name': 'Therp BV'})
-        partner.write({'contry_id': self.env.ref('base.be').id})
+        partner.write({'country_id': self.env.ref('base.be').id})
         self.assertEqual(
             partner.state_id,
             self.env.ref('l10n_nl_country_states.state_noordholland'),
         )
         partner.write({
-            'contry_id': self.env.ref('base.nl').id,
+            'country_id': self.env.ref('base.nl').id,
             'zip': '9711LM',
         })
         self.assertEqual(
