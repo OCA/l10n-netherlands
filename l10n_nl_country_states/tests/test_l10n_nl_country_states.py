0720405f2be72d9573b872f7e0de79807d6f54d9
Ronald Portier <ronald@therp.nl>
2018-02-26 15:43:06 +0100

    [FIX] l10n_nl_country_states. Better handling of countries outside of NL.

--- l10n_nl_country_states/tests/test_l10n_nl_country_states.py
+++ l10n_nl_country_states/tests/test_l10n_nl_country_states.py
@@ -4,27 +4,24 @@
 from openerp.tests.common import TransactionCase
 
 
-class TestL10nNlCountryStates(TransactionCase):
-    def test_l10n_nl_country_states(self):
+class TestDutchCountryStates(TransactionCase):
+    def test_dutch_country_states(self):
+        # Partner withouth country by default Dutch
         partner = self.env['res.partner'].create({
             'name': 'Therp',
-            'zip': '1053 NJ',
-        })
+            'zip': '1053 NJ'})
         self.assertEqual(
             partner.state_id,
-            self.env.ref('l10n_nl_country_states.state_noordholland'),
-        )
-        partner.write({'name': 'Therp BV'})
-        partner.write({'country_id': self.env.ref('base.be').id})
-        self.assertEqual(
-            partner.state_id,
-            self.env.ref('l10n_nl_country_states.state_noordholland'),
-        )
+            self.env.ref('l10n_nl_country_states.state_noordholland'))
+        # When moving partner to Belgium, state should be cleared
+        partner.write({
+            'name': 'Therp BV',
+            'country_id': self.env.ref('base.be').id})
+        self.assertEqual(partner.state_id.id, False)  # compare with id!
+        # Now move partner back to Netherlands/Groningen
         partner.write({
             'country_id': self.env.ref('base.nl').id,
-            'zip': '9711LM',
-        })
+            'zip': '9711LM'})
         self.assertEqual(
             partner.state_id,
-            self.env.ref('l10n_nl_country_states.state_groningen'),
-        )
+            self.env.ref('l10n_nl_country_states.state_groningen'))
