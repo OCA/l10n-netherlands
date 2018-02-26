64427c989611fe665e87e62a1ee43404b0153e27
Ronald Portier <ronald@therp.nl>
2018-02-26 15:43:06 +0100

    [FIX] l10n_nl_country_states. Better handling of countries outside of NL.

--- l10n_nl_country_states/README.rst
+++ l10n_nl_country_states/README.rst
@@ -63,10 +63,16 @@ Data
 Contributors
 ------------
 
-* lfreeke <lfreeke@therp.nl>
+* Lara Freeke <lfreeke@therp.nl>
 * Holger Brunn <hbrunn@therp.nl>
+* Ronald Portier <ronald@therp.nl>
 
-Do not contact contributors directly about help with questions or problems concerning this addon, but use the `community mailing list <mailto:community@mail.odoo.com>`_ or the `appropriate specialized mailinglist <https://odoo-community.org/groups>`_ for help, and the bug tracker linked in `Bug Tracker`_ above for technical issues.
+Do not contact contributors directly about help with questions or problems
+concerning this addon, but use the
+`community mailing list <mailto:community@mail.odoo.com>`_ or the
+`appropriate specialized mailinglist <https://odoo-community.org/groups>`_
+for help, and the bug tracker linked in `Bug Tracker`_
+above for technical issues.
 
 Maintainer
 ----------
--- l10n_nl_country_states/models/res_partner.py
+++ l10n_nl_country_states/models/res_partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2018 Therp BV <http://therp.nl>
+# Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp import api, models
 
@@ -8,38 +8,56 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     @api.model
+    def _get_state_for_zip(self, country_id, state_id, zip):
+        """Get state for zip.
+
+        If not Netherlands, just return present state, unless state is
+        in the Netherlands. In that case state will be False.
+        """
+        country_nl = self.env.ref('base.nl')
+        dutch_state_model = self.env['res.country.state.nl.zip']
+        state_model = self.env['res.country.state']
+        no_state = state_model.browse([])
+        if country_id and country_id != country_nl:
+            if state_id:
+                dutch_state = dutch_state_model.search([
+                    ('state_id', '=', state_id.id)], limit=1)
+                if dutch_state:
+                    return no_state
+            return state_id
+        # Assume netherlands
+        # Check wether we can find state for zip, if we find one, any
+        # state present in record will be overwritten
+        zip_digits = int(
+            filter(unicode.isdigit, unicode(zip or '')) or 0)
+        if not zip_digits:
+            return state_id
+        return dutch_state_model.search([
+            ('min_zip', '<=', zip_digits),
+            ('max_zip', '>=', zip_digits)], limit=1).state_id
+
+    @api.multi
+    def _set_state_from_zip(self):
+        """If country is not set, we assume it to be the Netherlands."""
+        for this in self:
+            state_id = self._get_state_for_zip(
+                this.country_id, this.state_id, this.zip)
+            if state_id != this.state_id:
+                super(ResPartner, this).write({'state_id': state_id.id})
+
+    @api.model
     def create(self, vals):
-        vals = self._state_from_vals(vals)
-        return super(ResPartner, self).create(vals)
+        result = super(ResPartner, self).create(vals)
+        result._set_state_from_zip()
+        return result
 
     @api.multi
     def write(self, vals):
-        vals = self._state_from_vals(vals)
-        return super(ResPartner, self).write(vals)
-
-    @api.model
-    def _state_from_vals(self, vals):
-        if 'zip' not in vals or 'state_id' in vals:
-            return vals
-        if 'country_id' in vals and vals['country_id'] != self.env.ref(
-                'base.nl'
-        ).id:
-            return vals
-        zip_digits = int(
-            filter(unicode.isdigit, unicode(vals['zip'] or '')) or 0
-        )
-        return dict(
-            vals,
-            state_id=self.env['res.country.state.nl.zip'].search([
-                ('min_zip', '<=', zip_digits),
-                ('max_zip', '>=', zip_digits),
-            ]).state_id.id,
-        )
+        result = super(ResPartner, self).write(vals)
+        self._set_state_from_zip()
+        return result
 
     @api.onchange('zip')
     def onchange_zip_country_id(self):
-        state_id = self._state_from_vals({
-            'zip': self.zip,
-            'country_id': self.country_id.id,
-        }).get('state_id')
-        self.state_id = self.env['res.country.state'].browse(state_id or [])
+        self.state_id = self._get_state_for_zip(
+            self.country_id, self.state_id, self.zip)
