410c7de2b9c105e4868484efa7a414d4eefb5ef0
Andrea <a.stirpe@onestein.nl>
2018-06-30 10:05:55 +0200

    Update version + Pylint

--- l10n_nl_xaf_auditfile_export/__manifest__.py
+++ l10n_nl_xaf_auditfile_export/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "XAF auditfile export",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "author": "Therp BV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Accounting & Finance",
