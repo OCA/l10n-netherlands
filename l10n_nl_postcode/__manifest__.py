294ffbeffe7dd9dbd2ddaf2fb22500c564c4a5e4
Andrea <a.stirpe@onestein.nl>
2018-07-19 16:12:57 +0200

    Add l10n_nl_postcode for V11

---
+++ l10n_nl_postcode/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016-2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Dutch postcode validation for Partners',
+    'version': '11.0.1.0.0',
+    'category': 'Localization',
+    'author': 'Onestein,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-netherlands',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'external_dependencies': {
+        'python': ['stdnum'],
+    },
+}
