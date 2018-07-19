f6e7d366c54ad501a70e42d8dbcfa1bc3e14ea7b
Holger Brunn <hbrunn@therp.nl>
2018-07-19 11:42:07 +0200

    [10.0] l10n_nl_partner_name improvements (#128)
    * [DEL] obsolete manifest
    
    * [IMP] allow to fill in names in a more natural way

--- l10n_nl_partner_name/__manifest__.py
+++ l10n_nl_partner_name/__manifest__.py
@@ -3,18 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Dutch partner names',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'author': 'Therp BV, Odoo Community Association (OCA)',
     'category': 'Contact management',
     'depends': [
         'partner_firstname',
+        'base_view_inheritance_extension',
     ],
     'data': [
         'view/res_partner.xml',
+        "data/ir.config_parameter.xml",
     ],
     'auto_install': False,
     'installable': True,
     'external_dependencies': {
         'python': ['mako'],
     },
+    'license': 'AGPL-3',
 }
