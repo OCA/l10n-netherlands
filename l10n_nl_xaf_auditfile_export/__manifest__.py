d7f153f78fc5c039a646592dbe345a2488d25f12
Andrea <a.stirpe@onestein.nl>
2018-08-01 09:55:34 +0200

    [IMP] Add option for auditfile compatibility in Unit4

--- l10n_nl_xaf_auditfile_export/__manifest__.py
+++ l10n_nl_xaf_auditfile_export/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "XAF auditfile export",
-    "version": "11.0.1.2.1",
+    "version": "11.0.1.3.0",
     "author": "Therp BV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Accounting & Finance",
