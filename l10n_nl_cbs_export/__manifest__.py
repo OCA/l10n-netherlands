c5bbf5edc360445add3b7f279522ef6ef54e93d8
Andrea <a.stirpe@onestein.nl>
2018-10-31 13:49:33 +0100

    Bump version

--- l10n_nl_cbs_export/__manifest__.py
+++ l10n_nl_cbs_export/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "CBS Export Intrahandel Sale",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "author": "Odoo Experts, Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-netherlands",
     "category": "Accounting",
