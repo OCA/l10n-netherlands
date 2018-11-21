85ee895f2fafdf851c36759b31886fdd3154f1a6
George Daramouskas <gdaramouskas@therp.nl>
2018-11-21 17:32:31 +0100

    [MIG] l10n_nl_partner_name: Migration to 11.0

--- l10n_nl_partner_name/__manifest__.py
+++ l10n_nl_partner_name/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-# © 2017 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2017 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Dutch partner names',
-    'version': '10.0.1.1.0',
+    'version': '11.0.0.0.0',
     'author': 'Therp BV, Odoo Community Association (OCA)',
     'category': 'Contact management',
     'depends': [
