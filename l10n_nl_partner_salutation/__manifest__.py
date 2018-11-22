36153b83de3fcc108458016d16ba228df5e6cf75
George Daramouskas <gdaramouskas@therp.nl>
2018-11-22 11:11:35 +0100

    [MIG] l10n_nl_partner_salutation: Migration to 11.0

--- l10n_nl_partner_salutation/__manifest__.py
+++ l10n_nl_partner_salutation/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Full salutation for partners, Dutch style',
-    'version': '10.0.1.1.0',
+    'version': '11.0.0.0.0',
     'author': 'Therp BV, Odoo Community Association (OCA)',
     'category': 'Contact management',
     'license': 'AGPL-3',
