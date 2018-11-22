952da76a73597e86430c7962cece5a2af2539d9a
George Daramouskas <gdaramouskas@therp.nl>
2018-11-22 11:11:35 +0100

    [MIG] l10n_nl_partner_salutation: Migration to 11.0

--- l10n_nl_partner_salutation/__init__.py
+++ l10n_nl_partner_salutation/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Therp BV <https://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import models
