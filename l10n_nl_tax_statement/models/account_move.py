6b1232139e8819b568455c6304f12b553418ab9b
Andrea <a.stirpe@onestein.nl>
2019-02-08 14:31:49 +0100

    [FIX] Include past invoices in report
    View past invoices moves when country is not NL
    
    [FIX] Button icon
    
    [FIX] Sort past invoices by date
    
    [FIX] Button icon
    
    [FIX] Domain condition
    
    Update roadmap
    
    Auto-update statement lines when adding/removing past invoices
    
    Fix failing test
    
    Coverage
    
    Fix calculation of past invoices in report

--- l10n_nl_tax_statement/models/account_move.py
+++ l10n_nl_tax_statement/models/account_move.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2018 Onestein (<https://www.onestein.eu>)
+# Copyright 2017-2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -15,10 +15,17 @@ class AccountMove(models.Model):
         'Include in VAT Statement'
     )
 
-    def add_move_in_statement(self):
-        for move in self:
-            move.l10n_nl_vat_statement_include = True
+    def l10n_nl_add_move_in_statement(self):
+        self.write({'l10n_nl_vat_statement_include': True})
+        self._l10n_nl_statement_update()
 
-    def unlink_move_from_statement(self):
-        for move in self:
-            move.l10n_nl_vat_statement_include = False
+    def l10n_nl_unlink_move_from_statement(self):
+        self.write({'l10n_nl_vat_statement_include': False})
+        self._l10n_nl_statement_update()
+
+    def _l10n_nl_statement_update(self):
+        model = self.env.context.get('params', {}).get('model', '')
+        obj_id = self.env.context.get('params', {}).get('id')
+        if model == 'l10n.nl.vat.statement' and isinstance(obj_id, int):
+            statement = self.env['l10n.nl.vat.statement'].browse(obj_id)
+            statement.statement_update()
