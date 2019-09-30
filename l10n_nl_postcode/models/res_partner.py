8cad1335df782962c127f2b3123f43ef4a0348d0
Andrea <a.stirpe@onestein.nl>
2019-09-30 16:09:30 +0200

    [13.0][MIG] l10n_nl_postcode

--- l10n_nl_postcode/models/res_partner.py
+++ l10n_nl_postcode/models/res_partner.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 Onestein (<http://www.onestein.eu>)
+# Copyright 2016-2020 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import logging
@@ -9,35 +9,27 @@ _logger = logging.getLogger(__name__)
 try:
     from stdnum.nl import postcode
 except ImportError:
-    _logger.debug('Cannot import stdnum.nl.postcode.')
+    _logger.debug("Cannot import stdnum.nl.postcode.")
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    @api.multi
     def _l10n_nl_postcode_get_warning(self):
         self.ensure_one()
-        msg = _('The Postcode you entered (%s) is not valid.')
-        warning = {
-            'title': _('Warning!'),
-            'message': msg % self.zip,
-        }
-        return warning
+        msg = _("The Postcode you entered (%s) is not valid.")
+        return {"title": _("Warning!"), "message": msg % self.zip}
 
-    @api.multi
     def _l10n_nl_postcode_check_country(self):
         self.ensure_one()
-        country = self.country_id
-        if not country or country != self.env.ref('base.nl'):
+        if not self.country_id or self.country_id != self.env.ref("base.nl"):
             return False
         return True
 
-    @api.multi
-    @api.onchange('zip', 'country_id')
+    @api.onchange("zip", "country_id")
     def onchange_zip_l10n_nl_postcode(self):
         # if 'skip_postcode_check' passed in context: will disable the check
-        if self.env.context.get('skip_postcode_check'):
+        if self.env.context.get("skip_postcode_check"):
             return
 
         if self.zip and self._l10n_nl_postcode_check_country():
@@ -48,4 +40,4 @@ class ResPartner(models.Model):
             else:
                 # display a warning
                 warning_msg = self._l10n_nl_postcode_get_warning()
-                return {'warning': warning_msg, }
+                return {"warning": warning_msg}
