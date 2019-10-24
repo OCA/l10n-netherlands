33fa2dd42d5cb8ddeeeda78b6a1f046cc959c0ea
Andrea <a.stirpe@onestein.nl>
2019-10-24 15:58:03 +0200

    [13.0][MIG] l10n_nl_account_tax_unece

--- l10n_nl_account_tax_unece/post_install.py
+++ l10n_nl_account_tax_unece/post_install.py
@@ -3,13 +3,11 @@
 # Copyright 2017-2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 
 
 def set_unece_on_taxes(cr, _):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        for company in env['res.company'].search([]):
-            if company.country_id and company.country_id != env.ref('base.nl'):
-                continue
+        for company in env["res.company"].search([]):
             company._l10n_nl_set_unece_on_taxes()
