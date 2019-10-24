e987877b47bd8945fd9a5f3fd499082924c59f9d
Andrea <a.stirpe@onestein.nl>
2019-10-24 15:58:03 +0200

    [13.0][MIG] l10n_nl_account_tax_unece

--- l10n_nl_account_tax_unece/tests/test_nl_account_tax_unece.py
+++ l10n_nl_account_tax_unece/tests/test_nl_account_tax_unece.py
@@ -2,32 +2,29 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import TransactionCase
+
 from ..post_install import set_unece_on_taxes
 
 
 class TestNlAccountTaxUnece(TransactionCase):
-
     def setUp(self):
         super().setUp()
 
-        self.my_company = self.env['res.company'].create({
-            'name': 'My Dutch Company',
-            'country_id': self.env.ref('base.nl').id,
-        })
-        l10nnl_chart_template = self.env.ref('l10n_nl.l10nnl_chart_template')
+        self.my_company = self.env["res.company"].create({"name": "My Dutch Company"})
+        l10nnl_chart_template = self.env.ref("l10n_nl.l10nnl_chart_template")
         old_company = self.env.user.company_id
         self.env.user.company_id = self.my_company.id
         l10nnl_chart_template.try_loading_for_current_company()
         self.env.user.company_id = old_company.id
 
-        self.unece_type_id = self.env.ref('account_tax_unece.tax_type_vat').id
+        self.unece_type_id = self.env.ref("account_tax_unece.tax_type_vat").id
         self.unece_categ_ids = [
-            self.env.ref('account_tax_unece.tax_categ_h').id,
-            self.env.ref('account_tax_unece.tax_categ_aa').id,
-            self.env.ref('account_tax_unece.tax_categ_z').id,
-            self.env.ref('account_tax_unece.tax_categ_s').id,
-            self.env.ref('account_tax_unece.tax_categ_b').id,
-            self.env.ref('account_tax_unece.tax_categ_e').id
+            self.env.ref("account_tax_unece.tax_categ_h").id,
+            self.env.ref("account_tax_unece.tax_categ_aa").id,
+            self.env.ref("account_tax_unece.tax_categ_z").id,
+            self.env.ref("account_tax_unece.tax_categ_s").id,
+            self.env.ref("account_tax_unece.tax_categ_b").id,
+            self.env.ref("account_tax_unece.tax_categ_e").id,
         ]
 
     def test_load_coa(self):
@@ -35,9 +32,9 @@ class TestNlAccountTaxUnece(TransactionCase):
         unece_type_id = self.unece_type_id
         unece_categ_ids = self.unece_categ_ids
 
-        taxes = self.env['account.tax'].search([
-            ('company_id', '=', self.my_company.id)
-        ])
+        taxes = self.env["account.tax"].search(
+            [("company_id", "=", self.my_company.id)]
+        )
 
         # For each tax account verify that the UNECE values are set
         for tax in taxes:
@@ -46,9 +43,9 @@ class TestNlAccountTaxUnece(TransactionCase):
 
     def test_existing_coa_update(self):
 
-        taxes = self.env['account.tax'].search([
-            ('company_id', '=', self.my_company.id)
-        ])
+        taxes = self.env["account.tax"].search(
+            [("company_id", "=", self.my_company.id)]
+        )
 
         # For each tax account, force the UNECE values to False
         for tax in taxes:
@@ -68,12 +65,8 @@ class TestNlAccountTaxUnece(TransactionCase):
 
     def test_post_init_hook(self):
 
-        self.env['res.company'].create({
-            'name': 'My New Dutch Company',
-            'country_id': self.env.ref('base.nl').id,
-        })
-        self.env['res.company'].create({
-            'name': 'My New Company',
-            'country_id': self.env.ref('base.it').id,
-        })
+        self.env["res.company"].create({"name": "My New Dutch Company"})
+        self.env["res.company"].create(
+            {"name": "My New Company", "country_id": self.env.ref("base.it").id}
+        )
         set_unece_on_taxes(self.cr, self.env)
