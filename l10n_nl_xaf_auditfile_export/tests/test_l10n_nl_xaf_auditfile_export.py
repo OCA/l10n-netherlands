3cd90129a93b2f00c694967bf704012dee5feaa2
Andrea <a.stirpe@onestein.nl>
2020-01-17 13:22:02 +0100

    pre-commit

--- l10n_nl_xaf_auditfile_export/tests/test_l10n_nl_xaf_auditfile_export.py
+++ l10n_nl_xaf_auditfile_export/tests/test_l10n_nl_xaf_auditfile_export.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import base64
-from io import BytesIO
 import os
+from io import BytesIO
 from zipfile import ZipFile
 
 from odoo.tests.common import TransactionCase
@@ -11,10 +11,9 @@ from odoo.tools import mute_logger
 
 
 class TestXafAuditfileExport(TransactionCase):
-
     def test_01_default_values(self):
-        ''' Check that the default values are filled on creation '''
-        record = self.env['xaf.auditfile.export'].create({})
+        """ Check that the default values are filled on creation """
+        record = self.env["xaf.auditfile.export"].create({})
 
         self.assertTrue(record)
         self.assertTrue(record.name)
@@ -28,8 +27,8 @@ class TestXafAuditfileExport(TransactionCase):
         self.assertFalse(record.unit4)
 
     def test_02_export_success(self):
-        ''' Do a basic auditfile export '''
-        record = self.env['xaf.auditfile.export'].create({})
+        """ Do a basic auditfile export """
+        record = self.env["xaf.auditfile.export"].create({})
         record.button_generate()
 
         self.assertTrue(record.name)
@@ -43,17 +42,15 @@ class TestXafAuditfileExport(TransactionCase):
         self.assertFalse(record.unit4)
 
         zf = BytesIO(base64.b64decode(record.auditfile))
-        with ZipFile(zf, 'r') as archive:
+        with ZipFile(zf, "r") as archive:
             filelist = archive.filelist
             contents = archive.read(filelist[-1]).decode()
-        self.assertTrue(contents.startswith('<?xml '))
+        self.assertTrue(contents.startswith("<?xml "))
 
-    @mute_logger(
-        'odoo.addons.l10n_nl_xaf_auditfile_export.models.xaf_auditfile_export'
-    )
+    @mute_logger("odoo.addons.l10n_nl_xaf_auditfile_export.models.xaf_auditfile_export")
     def test_03_export_error(self):
-        ''' Failure to export an auditfile '''
-        record = self.env['xaf.auditfile.export'].create({})
+        """ Failure to export an auditfile """
+        record = self.env["xaf.auditfile.export"].create({})
         record.company_id.country_id = False
         record.button_generate()
 
@@ -69,8 +66,8 @@ class TestXafAuditfileExport(TransactionCase):
         self.assertFalse(record.unit4)
 
     def test_04_export_success_unit4(self):
-        ''' Do a basic auditfile export (no Unit4) '''
-        record = self.env['xaf.auditfile.export'].create({})
+        """ Do a basic auditfile export (no Unit4) """
+        record = self.env["xaf.auditfile.export"].create({})
         record.unit4 = True
         record.button_generate()
 
@@ -84,16 +81,16 @@ class TestXafAuditfileExport(TransactionCase):
         self.assertTrue(record.fiscalyear_name)
         self.assertTrue(record.unit4)
 
-        if record.auditfile_name[-4:] == '.zip':
+        if record.auditfile_name[-4:] == ".zip":
             zf = BytesIO(base64.b64decode(record.auditfile))
-            with ZipFile(zf, 'r') as archive:
+            with ZipFile(zf, "r") as archive:
                 filelist = archive.filelist
                 contents = archive.read(filelist[-1]).decode()
-            self.assertTrue(contents.startswith('<?xml '))
+            self.assertTrue(contents.startswith("<?xml "))
 
     def test_05_export_success(self):
-        ''' Export auditfile with / character in filename '''
-        record = self.env['xaf.auditfile.export'].create({})
-        record.name += '%s01' % os.sep
+        """ Export auditfile with / character in filename """
+        record = self.env["xaf.auditfile.export"].create({})
+        record.name += "%s01" % os.sep
         record.button_generate()
         self.assertTrue(record)
