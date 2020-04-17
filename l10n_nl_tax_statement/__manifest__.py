27cfb945d00bab8dfab04ab5781db3cd2cc8bdaf
Andrea <a.stirpe@onestein.nl>
2020-04-17 14:04:30 +0200

    Pre-commit

--- l10n_nl_tax_statement/__manifest__.py
+++ l10n_nl_tax_statement/__manifest__.py
@@ -2,24 +2,22 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Netherlands BTW Statement',
-    'version': '12.0.1.1.0',
-    'category': 'Localization',
-    'license': 'AGPL-3',
-    'author': 'Onestein, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-netherlands',
-    'depends': [
-        'account_tax_balance',
+    "name": "Netherlands BTW Statement",
+    "version": "12.0.1.1.0",
+    "category": "Localization",
+    "license": "AGPL-3",
+    "author": "Onestein, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-netherlands",
+    "depends": ["account_tax_balance",],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/tax_statement_security_rule.xml",
+        "data/paperformat.xml",
+        "templates/assets.xml",
+        "views/l10n_nl_vat_statement_view.xml",
+        "views/report_tax_statement.xml",
+        "report/report_tax_statement.xml",
+        "wizard/l10n_nl_vat_statement_config_wizard.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/tax_statement_security_rule.xml',
-        'data/paperformat.xml',
-        'templates/assets.xml',
-        'views/l10n_nl_vat_statement_view.xml',
-        'views/report_tax_statement.xml',
-        'report/report_tax_statement.xml',
-        'wizard/l10n_nl_vat_statement_config_wizard.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
