88500cd263769082386234882884d1d6cb26c889
Jan Verbeek <jverbeek@therp.nl>
2020-05-26 17:29:37 +0200

    fixup! [FIX] Clean up l10n_nl_letsencrypt_transip_nl

--- l10n_nl_letsencrypt_transip_nl/tests/test_letsencrypt_transip_nl.py
+++ l10n_nl_letsencrypt_transip_nl/tests/test_letsencrypt_transip_nl.py
@@ -56,6 +56,7 @@ class TestLetsencryptTransip(TransactionCase):
         entry.type = "TXT"
         entry.name = "_acme-challenge"
         client.get_info.return_value.dnsEntries = [entry]
+        client.get_domain_names.return_value = ["example.com", "example.org"]
 
         def check_add_call(domain, entries):
             self.assertEquals(domain, "example.com")
@@ -75,4 +76,30 @@ class TestLetsencryptTransip(TransactionCase):
         )
         client.get_info.assert_called()
         client.remove_dns_entries.assert_called_with("example.com", [entry])
+        client.add_dns_entries.assert_called()
+
+    @patch("transip.service.domain.DomainService")
+    def test_invocation_with_subdomain(self, domain_service):
+        client = domain_service.return_value
+
+        client.get_info.return_value.dnsEntries = []
+        client.get_domain_names.return_value = ["example.com", "example.org"]
+
+        def check_add_call(domain, entries):
+            self.assertEquals(domain, "example.com")
+            self.assertEquals(len(entries), 1)
+            self.assertEquals(entries[0].name, "_acme-challenge.foo.bar")
+            self.assertEquals(entries[0].type, "TXT")
+            self.assertEquals(entries[0].content, "a_token")
+
+        client.add_dns_entries.side_effect = check_add_call
+
+        self.env["letsencrypt"]._respond_challenge_dns_transip(
+            "foo.bar.example.com", "a_token"
+        )
+
+        domain_service.assert_called_with(
+            login="simulacra", private_key="somekey"
+        )
+        client.get_info.assert_called()
         client.add_dns_entries.assert_called()
