b83f60048a7b4b14885350ae47c779b62e53f299
Jan Verbeek <jverbeek@therp.nl>
2020-05-27 11:47:02 +0200

    fixup! [FIX] Clean up l10n_nl_letsencrypt_transip_nl

--- l10n_nl_letsencrypt_transip_nl/__manifest__.py
+++ l10n_nl_letsencrypt_transip_nl/__manifest__.py
@@ -2,12 +2,12 @@
 # Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Letsencrypt Transip NL",
+    "name": "Let's Encrypt TransIP integration",
     "version": "10.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Administration",
-    "summary": "Adds support for transip.nl",
+    "summary": "Use TransIP for Let's Encrypt DNS validation",
     "depends": [
         "letsencrypt",
     ],
