62cd9c64ed26c0aeaea2204849b833a24a615fc4
Andrea <a.stirpe@onestein.nl>
2020-10-22 11:30:19 +0200

    [14.0][MIG] l10n_nl_postcode

--- l10n_nl_postcode/__manifest__.py
+++ l10n_nl_postcode/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Dutch postcode validation for Partners",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Localization",
     "author": "Onestein,Odoo Community Association (OCA)",
     "maintainers": ["astirpe"],
