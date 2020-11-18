19b8da589bf03efe663a3212b7dd93cf925e5507
Tom Blauwendraat <tom@sunflowerweb.nl>
2020-11-18 10:11:32 +0100

    fixup! [FIX] Problems with the module

--- l10n_nl_base_ubl/models/base_ubl.py
+++ l10n_nl_base_ubl/models/base_ubl.py
@@ -10,37 +10,34 @@ class BaseUbl(models.AbstractModel):
     _inherit = "base.ubl"
 
     @api.model
-    def _ubl_add_party_legal_entity(
-            self, partner, parent_node, ns, version="2.1"):
+    def _ubl_add_party_legal_entity(self, partner, parent_node, ns, version="2.1"):
         """ Add NL-specific things to PartyLegalEntity """
         res = super()._ubl_add_party_legal_entity(
             partner, parent_node, ns, version=version
         )
         # PartyLegalEntity/CompanyID must be added just after RegistrationName
         party_legal_entity = parent_node.find(ns["cac"] + "PartyLegalEntity")
-        registration_name = party_legal_entity.find(
-            ns["cbc"] + "RegistrationName")
+        registration_name = party_legal_entity.find(ns["cbc"] + "RegistrationName")
         id_dict = self._ubl_get_party_identification(partner)
         if id_dict:
             for scheme_name, party_id_text in id_dict.iteritems():
                 company_id = etree.Element(
-                    ns['cbc'] + 'CompanyID', schemeName=scheme_name)
+                    ns["cbc"] + "CompanyID", schemeName=scheme_name
+                )
                 company_id.text = party_id_text
                 registration_name.addnext(company_id)
         return res
 
     @api.model
     def _ubl_get_party_identification(self, commercial_partner):
-        res = super(BaseUbl, self)._ubl_get_party_identification(
-            commercial_partner
-        )
+        res = super(BaseUbl, self)._ubl_get_party_identification(commercial_partner)
         oin = self._l10n_nl_base_ubl_get_oin(commercial_partner)
         kvk = self._l10n_nl_base_ubl_get_kvk(commercial_partner)
         # OIN (0190) trumps KVK number (0106) if it is filled for a partner
         if oin:
-            res['0190'] = oin
+            res["0190"] = oin
         elif kvk:
-            res['0106'] = kvk
+            res["0106"] = kvk
         return res
 
     def _l10n_nl_base_ubl_get_kvk(self, partner):
