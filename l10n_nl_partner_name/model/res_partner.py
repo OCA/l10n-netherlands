785a1ce315d8170b097368accb3d5477d2eb79dc
Ronald Portier <ronald@therp.nl>
2022-01-13 17:21:10 +0100

    [IMP] l10n_nl_partner_name: black, isort, prettier

--- l10n_nl_partner_name/model/res_partner.py
+++ l10n_nl_partner_name/model/res_partner.py
@@ -1,12 +1,14 @@
 # Copyright 2017 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api
+from odoo import api, fields, models
+
 from odoo.addons.mail.models.mail_template import mako_safe_template_env
 
 
 class ResPartner(models.Model):
     """Extend res.partner with extra fields for Dutch names."""
-    _inherit = 'res.partner'
+
+    _inherit = "res.partner"
 
     initials = fields.Char()
     infix = fields.Char()
@@ -16,26 +18,29 @@ class ResPartner(models.Model):
     def _compute_name(self):
         for record in self:
             record.name = record._get_computed_name(
-                record.lastname, record.firstname, record.initials,
-                record.infix)
+                record.lastname, record.firstname, record.initials, record.infix
+            )
 
     @api.onchange("firstname", "lastname", "initials", "infix")
     def _onchange_subnames(self):
         return super(ResPartner, self)._onchange_subnames()
 
     @api.model
-    def _get_computed_name(self, lastname, firstname, initials=None,
-                           infix=None):
+    def _get_computed_name(self, lastname, firstname, initials=None, infix=None):
         name_template = mako_safe_template_env.from_string(
             self.env.context.get(
-                'name_format',
+                "name_format",
                 "${firstname or initials or ''}"
                 "${(firstname or initials) and ' ' or ''}"
-                "${infix or ''}${infix and ' ' or ''}${lastname or ''}"))
-        name = name_template.render({
-            'firstname': firstname,
-            'lastname': lastname,
-            'initials': initials,
-            'infix': infix,
-        })
-        return name if name else ''
+                "${infix or ''}${infix and ' ' or ''}${lastname or ''}",
+            )
+        )
+        name = name_template.render(
+            {
+                "firstname": firstname,
+                "lastname": lastname,
+                "initials": initials,
+                "infix": infix,
+            }
+        )
+        return name if name else ""
