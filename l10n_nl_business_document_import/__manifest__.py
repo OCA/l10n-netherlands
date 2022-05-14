3f23b01a23290493df432af275e21ee22f092259
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2022-05-14 14:34:05 +0200

    add l10n_nl_business_document_import

---
+++ l10n_nl_business_document_import/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 bosd
+# @author: bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "l10n NL Business Document Import",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Localization/Netherlands",
+    "license": "AGPL-3",
+    "summary": "Adapt the module base_business_document_import for the Netherlands",
+    "author": "bosd,Odoo Community Association (OCA)",
+    "maintainers": ["bosd"],
+    "website": "https://github.com/OCA/l10n-netherlands",
+    "depends": ["partner_coc", "l10n_nl_oin", "base_business_document_import"],
+    "installable": True,
+    "auto_install": True,
+}
