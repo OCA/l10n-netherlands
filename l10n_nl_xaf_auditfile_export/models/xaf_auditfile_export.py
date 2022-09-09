3d0416f6409f8cb0f3c2ce22df145689f7fb7450
Tom Blauwendraat <tom@sunflowerweb.nl>
2022-09-09 13:01:35 +0200

    [IMP] Always store the output file, even if it's faulty

--- l10n_nl_xaf_auditfile_export/models/xaf_auditfile_export.py
+++ l10n_nl_xaf_auditfile_export/models/xaf_auditfile_export.py
@@ -78,6 +78,9 @@ class XafAuditfileExport(models.Model):
         "If you want to export an auditfile with the official standard "
         "(missing the Unit4 compatibility) just set this flag to False."
     )
+    auditfile_success = fields.Boolean(copy=False)
+    date_generated = fields.Datetime("Date generated", readonly=True, copy=False)
+    company_id = fields.Many2one("res.company", "Company", required=True)
 
     @api.model
     def default_get(self, fields_list):
@@ -137,10 +140,27 @@ class XafAuditfileExport(models.Model):
         auditfile = os.path.join(tmpdir, filename)
         archivedir = mkdtemp()
         archive = os.path.join(archivedir, filename)
+        self.auditfile_success = False
         try:
             with open(auditfile, "w+") as tmphandle:
                 tmphandle.write(xml)
             del xml
+
+            logging.getLogger(__name__).debug(
+                "Created an auditfile in %ss, using %sk memory",
+                int(time.time() - t0),
+                (memory_info() - m0) / 1024,
+            )
+
+            # Store in compressed format on the auditfile record
+            zip_path = shutil.make_archive(archive, "zip", tmpdir, verbose=True)
+            with open(zip_path, "rb") as auditfile_zip:
+                self.auditfile = base64.b64encode(auditfile_zip.read())
+            logging.getLogger(__name__).debug(
+                "Created an auditfile in %ss, using %sk memory",
+                int(time.time() - t0),
+                (memory_info() - m0) / 1024,
+            )
 
             # Validate the generated XML
             xsd = etree.XMLSchema(
@@ -157,20 +177,13 @@ class XafAuditfileExport(models.Model):
             xsd.assertValid(etree.parse(auditfile))
             del xsd
 
-            # Store in compressed format on the auditfile record
-            zip_path = shutil.make_archive(archive, "zip", tmpdir, verbose=True)
-            with open(zip_path, "rb") as auditfile_zip:
-                self.auditfile = base64.b64encode(auditfile_zip.read())
-            logging.getLogger(__name__).debug(
-                "Created an auditfile in %ss, using %sk memory",
-                int(time.time() - t0),
-                (memory_info() - m0) / 1024,
-            )
+            self.auditfile_success = True
 
         except (etree.XMLSyntaxError, etree.DocumentInvalid) as e:
             logging.getLogger(__name__).error(e)
             logging.getLogger(__name__).info(traceback.format_exc())
             self.message_post(body=e)
+
         finally:
             shutil.rmtree(tmpdir)
             shutil.rmtree(archivedir)
