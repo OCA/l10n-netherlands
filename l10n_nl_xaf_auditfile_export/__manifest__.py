9e8118e80ec08c3cb2caf547db3108b68ec31085
Ronald Portier (Therp BV) <ronald@therp.nl>
2022-10-11 13:23:16 +0200

    [MIG] l10n_nl_xaf_auditfile_export: Migration to 15.0

--- l10n_nl_xaf_auditfile_export/__manifest__.py
+++ l10n_nl_xaf_auditfile_export/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2015 Therp BV <https://therp.nl>
+# Copyright 2015-2022 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "XAF auditfile export",
-    "version": "14.0.1.3.0",
+    "version": "15.0.1.3.0",
     "author": "Therp BV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-netherlands",
     "license": "AGPL-3",
