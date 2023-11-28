1947af636eb274e6b26b18a838d5f833d9d61cff
Yung-Wa <yw.ng@onestein.nl>
2023-11-28 14:08:00 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (51 of 51 strings)
    
    Translation: l10n-netherlands-16.0/l10n-netherlands-16.0-l10n_nl_xaf_auditfile_export
    Translate-URL: https://translation.odoo-community.org/projects/l10n-netherlands-16-0/l10n-netherlands-16-0-l10n_nl_xaf_auditfile_export/nl/

--- l10n_nl_xaf_auditfile_export/__manifest__.py
+++ l10n_nl_xaf_auditfile_export/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "XAF auditfile export",
-    "version": "16.0.1.1.0",
+    "version": "16.0.1.1.1",
     "author": "Therp BV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-netherlands",
     "license": "AGPL-3",
