0b06b8c85fc3b14bfed512cea40c992eaf8833fb
Holger Brunn <mail@hunki-enterprises.com>
2026-03-07 08:42:36 +0100

    [MIG] l10n_nl_partner_name: Migration to 18.0

--- l10n_nl_partner_name/__manifest__.py
+++ l10n_nl_partner_name/__manifest__.py
@@ -2,13 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Dutch partner names",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "Adapt parter names to Dutch conventions (support infix)",
     "author": "Hunki Enterprises BV, Therp BV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-netherlands",
     "category": "Contact management",
     "depends": ["partner_firstname"],
-    "data": ["views/res_partner.xml", "data/ir.config_parameter.xml"],
+    "data": [
+        "views/res_partner.xml",
+        "views/res_users.xml",
+        "data/ir.config_parameter.xml",
+    ],
     "installable": True,
     "license": "AGPL-3",
 }
